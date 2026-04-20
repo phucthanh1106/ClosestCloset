d05e83cc2e79b7bf02a588d9c891d8f37edb689d
phucthanh1106 <tranpthanh2005@gmail.com>
2026-04-19 20:14:10 -0400

    update url for backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://closestcloset-backend.onrender.com",
         changeOrigin: true,
       },
     },
