94d53ff176dd647f1ba01fd2bc20bd51e79dbd54
phucthanh1106 <tranpthanh2005@gmail.com>
2026-04-20 00:37:35 -0400

    move .env to client folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// When you run npm run dev, Vite starts a local server on your Mac that handles Vite Proxy
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
