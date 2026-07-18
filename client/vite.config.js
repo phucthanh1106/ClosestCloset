f8c19dc1157243a262cf00396942d8d973a046ef
phucthanh1106 <tranpthanh2005@gmail.com>
2026-07-18 16:57:12 +0700

    rewrote docker-compose.yaml

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
 
   // Fallback to localhost if no specific VITE_PROXY_TARGET is provided
   const proxyTarget = env.VITE_PROXY_TARGET || "http://localhost:4000";
-  const chatbotTarget = env.VITE_PROXY_TARGET || "http://localhost:8000"
+  const chatbotTarget = env.VITE_CHATBOT_PROXY_TARGET || "http://localhost:8000";
 
   return {
     plugins: [react(), tailwindcss()],
