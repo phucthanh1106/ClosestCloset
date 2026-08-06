fdb7164f8d34b322aaad7214ecb2aebf8ed9d9a3
phucthanh1106 <tranpthanh2005@gmail.com>
2026-08-06 20:40:51 +0700

    changed cookie settings

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 3 * 24 * 60 * 60 * 1000,
 };
 
@@ -77,7 +77,7 @@ export const logoutUser = async (req, res) => {
     res.clearCookie("token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: "none",
     });
 
     res.status(200).json({
